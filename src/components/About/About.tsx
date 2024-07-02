123434c43a17955fa2dbda07f9f0c681b4779848
Vadim06 <vadim.mikhaylets@gmail.com>
2024-07-02 15:02:59 +0200

    slideshow v1

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import './About.css';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 import me from '../../img/me.jpg'
 import { AiOutlineHtml5 } from "react-icons/ai";
 import { ImCss3 } from "react-icons/im";
 import { TbBrandJavascript, TbBrandTypescript } from "react-icons/tb";
 import { BiLogoReact, BiGitBranch } from "react-icons/bi";
 import { RiBootstrapLine } from "react-icons/ri";
-let icons = [
+import { CiDumbbell } from "react-icons/ci";
+import { MdVideoSettings } from "react-icons/md";
+import { GiGuitar } from "react-icons/gi";
+import { FaCameraRetro } from "react-icons/fa";
+import { CarouselProvider, Slider, Slide, Image, ButtonBack, ButtonNext } from 'pure-react-carousel';
+const icons = [
     <AiOutlineHtml5 size={'5rem'} />,
     <ImCss3 size={'4rem'} />,
     <TbBrandJavascript size={'5rem'} />,
@@ -14,6 +20,20 @@ let icons = [
     <TbBrandTypescript size={'5rem'} />,
     <BiGitBranch size={'5rem'} />,
     <RiBootstrapLine size={'5rem'} />
+]
+
+const hobbyIcons = [
+    <CiDumbbell className='hobbyImg dumbbell' size={'10rem'} />,
+    <FaCameraRetro className='hobbyImg' size={'6.8rem'} />,
+    <MdVideoSettings className='hobbyImg' size={'6.6rem'} />,
+    <GiGuitar className='hobbyImg' size={'7rem'} />
+]
+
+const hobbyTexts = [
+    <p className='hobbyText'>Working out</p>,
+    <p className='hobbyText'>Photography</p>,
+    <p className='hobbyText'>Video editing</p>,
+    <p className='hobbyText'>Guitar</p>
 ]
 
 export const About = () => {
@@ -30,13 +50,30 @@ export const About = () => {
                         <br />
                         I enjoy learning new technologies, problem-solving and working in a team
                     </p>
-                    <h6>Some of my <span>hobbies</span></h6>
-                    <ul className='hobbies'>
-                        <li>Working out</li>
-                        <li>Photography</li>
-                        <li>Video editing</li>
-                        <li>Cycling</li>
-                    </ul>
+                    <div className='hobbies'>
+                        <h6>Some of my <span>hobbies</span></h6>
+                        <CarouselProvider
+                            naturalSlideWidth={60}
+                            naturalSlideHeight={20}
+                            totalSlides={4}
+                            isPlaying={true}
+                            infinite={true}
+                        >
+                            <Slider className='sliderHobby'>
+                                {hobbyIcons.map((icon, index) => (
+                                    <Slide key={index} index={index}>
+                                        {icon}
+                                        {hobbyTexts[index]}
+                                    </Slide>
+                                ))}
+                            </Slider>
+                            <div className='hobbyBtns'>
+                                <ButtonBack><span className='prev'></span></ButtonBack>
+                                <ButtonNext><span className='next'></span></ButtonNext>
+                            </div>
+                        </CarouselProvider>
+                    </div>
+
                 </div>
                 <div className='meContainer'>
                     <img src={me} alt="me" className='me' />
