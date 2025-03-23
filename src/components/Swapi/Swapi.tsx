d6b74ea80ca455798c78bb86768890837e71df43
Vadym <vadim.mikhaylets@gmail.com>
2025-03-23 10:50:47 +0100

    pagination

--- src/components/Swapi/Swapi.tsx
+++ src/components/Swapi/Swapi.tsx
@@ -7,37 +7,111 @@ interface Character {
     height: string;
     mass: string;
 }
+let showCharactersVar = false;
+let currentPage = 1;
 
 export const Swapi = () => {
     const [characters, setCharacters] = useState<Character[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
+    const [showCharacters, setShowCharacters] = useState<boolean>(false);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [pages, setPages] = useState<number>(0);
+    const [category, setCategory] = useState<string>("people");
 
-    useEffect(() => {
-        axios.get("https://swapi.dev/api/people/")
+    const toggleShowCharacter = () => {
+        showCharactersVar = !showCharactersVar;
+        if (showCharactersVar) {
+            setShowCharacters(true)
+            axios.get(`https://swapi.dev/api/${category}`)
+                .then(response => {
+                    setCharacters(response.data.results);
+                    console.log(response.data.results);
+                    setIsLoading(false);
+                    paginate(response.data.count);
+                })
+                .catch(error => {
+                    console.error("Error fetching data:", error);
+                    setIsLoading(false);
+                });
+        } else {
+            deleteList();
+        }
+    }
+
+    const deleteList = () => {
+        setIsLoading(true);
+        setCharacters([]);
+        setShowCharacters(false);
+        setPages(0);
+        currentPage = 1;
+    }
+
+    const paginate = (count: number) => {
+        if (count > 10) {
+            setPages((Math.ceil(count / 10)));
+        } else {
+            setPages(0);
+        }
+    }
+
+    const nextPage = () => {
+        currentPage++
+        setIsLoading(true);
+        axios.get(`https://swapi.dev/api/${category}/?page=${currentPage}`)
             .then(response => {
                 setCharacters(response.data.results);
-                setLoading(false);
+                console.log(response.data.results);
+                setIsLoading(false);
             })
             .catch(error => {
                 console.error("Error fetching data:", error);
-                setLoading(false);
+                setIsLoading(false);
             });
-    }, []);
+    }
 
-    return (<div className="">
-        <h1>Star Wars Characters</h1>
-        {loading ? (
-            <p>Loading...</p>
-        ) : (
-            <ul>
-                {characters.map((character, index) => (
-                    <li key={index}>
-                        <strong>{character.name}</strong> - Height: {character.height}cm, Mass: {character.mass}kg
-                    </li>
-                ))}
-            </ul>
-        )}
-    </div>
+    const prevPage = () => {
+        currentPage--
+        setIsLoading(true);
+        axios.get(`https://swapi.dev/api/${category}/?page=${currentPage}`)
+            .then(response => {
+                setCharacters(response.data.results);
+                console.log(response.data.results);
+                setIsLoading(false);
+            })
+            .catch(error => {
+                console.error("Error fetching data:", error);
+                setIsLoading(false);
+            });
+    }
+
+    return (
+        <section className="SWSection">
+            <div className='SWContainer'>
+                <h3>What would you like to see?</h3>
+                <select name="category" value={category} onChange={(event: React.ChangeEvent<HTMLSelectElement>) => { setCategory(event.target.value); deleteList() }} id="SWSelect">
+                    <option value="people">people</option>
+                    <option value="planets">planets</option>
+                </select>
+                <button onClick={toggleShowCharacter} className='showCharactersBtn'>{isLoading ? "show all characters" : "hide all characters"}</button>
+                {isLoading && showCharacters ? (
+                    <p>Loading...</p>
+                ) : (
+                    <ul>
+                        {characters.map((character, index) => (
+                            <li key={index}>
+                                <strong>{character.name}</strong> - Height: {character.height}cm, Mass: {character.mass}kg
+                            </li>
+                        ))}
+                    </ul>
+                )}
+                {pages > 0 && (
+                    <div>
+                        <p>There are {pages} pages</p>
+                        {currentPage > 1 && <button onClick={prevPage}>previous</button>}
+                        {currentPage < pages && <button onClick={nextPage}>next</button>}
+                    </div>
+                )}
+            </div>
+        </section>
     )
 }
 
